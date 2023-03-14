8547f0729fab6194125bd4131cf478c22fdce7c8
JavaBorka <borka34@gmail.com>
2023-03-14 08:11:10 +0100

    Cvičení 3

--- index.js
+++ index.js
@@ -1,1 +1,42 @@
 console.log('funguju!');
+
+const alarmElm = document.querySelector('.alarm')
+const audioElm = document.querySelector('audio')
+
+const ring = () => {
+    alarmElm.classList.add('alarm--ring')
+    audioElm.play()
+}
+let timeToring = prompt('Kolik vteřin má uplynout, než minutka začne zvonit?')
+timeToring = timeToring * 1000
+
+// VYPNUTÍ ČASOVAČE
+
+const timer = setTimeout(ring, timeToring)
+
+const stopTimeout = () => {
+    audioElm.pause()
+    clearTimeout(timer)
+}
+const stopAlarm = document.querySelector('.stopAlarm')
+stopAlarm.addEventListener('click', stopTimeout)
+
+
+/****** Řešení Monika *******/
+
+/*
+const audioElm = document.querySelector('audio')
+const alarmElm = document.querySelector('.alarm')
+const stopAlarm = document.querySelector('.stopAlarm')
+const vstupUzivatel = Number(prompt('Kolik vteřin má uplynout, než minutka začne zvonit?'))
+
+const timer = setTimeout(() => {
+    alarmElm.classList.add('alarm--ring')
+    audioElm.play()
+}, vstupUzivatel*1000)
+
+stopAlarm.addEventListener('click', () => {
+    audioElm.pause()
+    clearTimeout(timer)
+})
+*/
